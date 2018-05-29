7396df1fd66dffacb8bdefb67c899ea76f05dfee
Johan Sjöland <johansjoland@MacBook-Pro-3.local>
2018-05-29 11:59:36 +0200

    Fix posts

--- src/Post.php
+++ src/Post.php
@@ -16,19 +16,19 @@ class Post extends Model {
 
 	// ---
 
-	public function getFront() {
+	public static function getFront() {
 		self::$post = get_post(get_option('page_on_front'));
 		return self::prepared();
 	}
 
-	public function getPost($slug=NULL) {
+	public static function getPost($slug=NULL) {
 		self::$post = ($p = get_page_by_path($slug,OBJECT,['post','page'])) ? $p : NULL;
 		return self::prepared();
 	}
 
 	// ---
 
-	public function prepared() {
+	public static function prepared() {
 		if (empty(self::$post)) return NULL;
 		self::$post->prepared = TRUE;
 		return self::$post;
