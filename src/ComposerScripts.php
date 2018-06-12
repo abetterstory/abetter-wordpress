7a7294de4df00cf83559e9c0a5a9893e97733d4c
Johan Sjöland <johansjoland@MacBook-Pro-3.local>
2018-06-12 13:30:30 +0200

    Update scripts renamer to include voyager

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -21,10 +21,40 @@ class ComposerScripts {
 
     }
 
+	public static function renameHelperFunctionsVoyager(Event $event) {
+
+        $vendorDir   = $event->getComposer()->getConfig()->get('vendor-dir');
+        $helpersPath = $vendorDir . '/tcg/voyager/src/Helpers/helpersi18n.php';
+
+        if (!file_exists($helpersPath)) return;
+
+        $content = file_get_contents($helpersPath);
+        $content = str_replace("function_exists('__')", "function_exists('___')", $content);
+        $content = str_replace('function __(', 'function ___(', $content);
+
+        file_put_contents($helpersPath, $content);
+
+    }
+
 	public static function restoreHelperFunctions(Event $event) {
 
         $vendorDir   = $event->getComposer()->getConfig()->get('vendor-dir');
         $helpersPath = $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php';
+
+        if (!file_exists($helpersPath)) return;
+
+        $content = file_get_contents($helpersPath);
+        $content = str_replace("function_exists('___')", "function_exists('__')", $content);
+        $content = str_replace('function ___(', 'function __(', $content);
+
+        file_put_contents($helpersPath, $content);
+
+    }
+
+	public static function restoreHelperFunctionsVoyager(Event $event) {
+
+        $vendorDir   = $event->getComposer()->getConfig()->get('vendor-dir');
+		$helpersPath = $vendorDir . '/tcg/voyager/src/Helpers/helpersi18n.php';
 
         if (!file_exists($helpersPath)) return;
 
