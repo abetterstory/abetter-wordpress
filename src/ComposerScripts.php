e52c77ecf11ac5c405b8dd9d510c39d15879cae8
Johan Sjöland <johansjoland@MacBook-Pro-3.local>
2018-06-12 14:39:59 +0200

    Fix helper renaming and __ trans

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -6,64 +6,66 @@ use Composer\Script\Event;
 
 class ComposerScripts {
 
-    public static function renameHelperFunctions(Event $event) {
+	public static function renameHelperFunctions(Event $event) {
+		self::renameHelperFoundation($event);
+		self::renameHelperVoyager($event);
+		self::modifyHelperWordpress($event);
+	}
 
+	// ---
+
+	public static function modifyHelperWordpress(Event $event) {
+        $vendorDir   = $event->getComposer()->getConfig()->get('vendor-dir');
+        $helpersPath = $vendorDir . '/../resources/wordpress/core/wp-includes/l10n.php';
+        if (!file_exists($helpersPath)) return;
+        $content = file_get_contents($helpersPath);
+        $content = str_replace('function __( ', "if (!function_exists('__')){function __(", $content);
+		$content = str_replace('return translate( $text, $domain );', 'return translate($text,$domain);}', $content);
+        file_put_contents($helpersPath, $content);
+    }
+
+	// ---
+
+    public static function renameHelperFoundation(Event $event) {
         $vendorDir   = $event->getComposer()->getConfig()->get('vendor-dir');
         $helpersPath = $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php';
-
         if (!file_exists($helpersPath)) return;
-
         $content = file_get_contents($helpersPath);
         $content = str_replace("function_exists('__')", "function_exists('___')", $content);
         $content = str_replace('function __(', 'function ___(', $content);
-
         file_put_contents($helpersPath, $content);
-
     }
 
-	public static function renameHelperFunctionsVoyager(Event $event) {
-
+	public static function renameHelperVoyager(Event $event) {
         $vendorDir   = $event->getComposer()->getConfig()->get('vendor-dir');
         $helpersPath = $vendorDir . '/tcg/voyager/src/Helpers/helpersi18n.php';
-
         if (!file_exists($helpersPath)) return;
-
         $content = file_get_contents($helpersPath);
         $content = str_replace("function_exists('__')", "function_exists('___')", $content);
         $content = str_replace('function __(', 'function ___(', $content);
-
         file_put_contents($helpersPath, $content);
-
     }
 
-	public static function restoreHelperFunctions(Event $event) {
+	// ---
 
+	public static function restoreHelperFoundation(Event $event) {
         $vendorDir   = $event->getComposer()->getConfig()->get('vendor-dir');
         $helpersPath = $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php';
-
         if (!file_exists($helpersPath)) return;
-
         $content = file_get_contents($helpersPath);
         $content = str_replace("function_exists('___')", "function_exists('__')", $content);
         $content = str_replace('function ___(', 'function __(', $content);
-
         file_put_contents($helpersPath, $content);
-
     }
 
-	public static function restoreHelperFunctionsVoyager(Event $event) {
-
+	public static function restoreHelperVoyager(Event $event) {
         $vendorDir   = $event->getComposer()->getConfig()->get('vendor-dir');
 		$helpersPath = $vendorDir . '/tcg/voyager/src/Helpers/helpersi18n.php';
-
         if (!file_exists($helpersPath)) return;
-
         $content = file_get_contents($helpersPath);
         $content = str_replace("function_exists('___')", "function_exists('__')", $content);
         $content = str_replace('function ___(', 'function __(', $content);
-
         file_put_contents($helpersPath, $content);
-
     }
 
 }
--- src/helpers.php
+++ src/helpers.php
@@ -6,3 +6,34 @@ if (!function_exists('__')) {
         return trans($key, $par);
     }
 }
+
+/* resources/wordpress/core/wp-includes/l10n.php
+
+function __( $text, $domain = 'default' ) {
+    return translate( $text, $domain );
+}
+
+*/
+
+/* vendor/tcg/voyager/src/Helpers/helpersi18n.php
+
+if (!function_exists('___')) {
+    function ___($key, array $par = [])
+    {
+        return trans($key, $par);
+    }
+}
+
+*/
+
+/* vendor/laravel/framework/src/Illuminate/Foundation/helpers.php
+
+if (! function_exists('___')) {
+
+    function ___________($key, $replace = [], $locale = null)
+    {
+        return app('translator')->getFromJson($key, $replace, $locale);
+    }
+}
+
+*/
