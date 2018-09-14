cb71685e318ae2cec3993892a2c421dfdca3db99
Johan Sjöland <johansjoland@MacBook-Pro-3.local>
2018-09-14 11:14:26 +0200

    Edit html order of scripts and fonts

--- views/components/html/body/scripts.blade.php
+++ views/components/html/body/scripts.blade.php
@@ -1,14 +1,11 @@
 @php _debug('scripts');
+$site->scripts_manifest = 'scripts/manifest.js';
+$site->scripts_vendor = 'scripts/vendor.js';
 $site->scripts_app = 'scripts/app.js';
 $site->theme_js = _dictionary('theme_js',NULL,'');
 @endphp
 
-@if ($site->scripts_app)
-<script src="{{ url('/').mix($site->scripts_app) }}" type="text/javascript"></script>
-@endif
-
-@if ($site->theme_js)
-<script>
-{!! $site->theme_js !!}
-</script>
-@endif
+@if (!empty($site->scripts_manifest))<script src="{{ url('/').mix($site->scripts_manifest) }}" type="text/javascript"></script>@endif
+@if (!empty($site->scripts_vendor))<script src="{{ url('/').mix($site->scripts_vendor) }}" type="text/javascript"></script>@endif
+@if (!empty($site->scripts_app))<script src="{{ url('/').mix($site->scripts_app) }}" type="text/javascript"></script>@endif
+@if (!empty($site->theme_js))<script>{!! $site->theme_js !!}</script>@endif
