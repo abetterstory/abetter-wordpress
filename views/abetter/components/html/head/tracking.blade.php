5a56155be311979a89722de31b295abad0d09151
Johan Sjöland <johansjoland@MacBook-Pro-3.local>
2018-10-10 14:45:04 +0200

    Update tracker

--- views/abetter/components/html/head/tracking.blade.php
+++ views/abetter/components/html/head/tracking.blade.php
@@ -1,24 +1,8 @@
 @php _debug('tracking');
-$site->ga = "";
-$site->ga_tagmanager = "";
+$site->ga = ($ga = env('APP_GA')) ? $ga : 'UA-127222126-1'; // ABetter Dev
 @endphp
 
-@if (!empty($site->ga_tagmanager))
-<script>
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{{ $site->ga_tagmanager }}');
-</script>
-@endif
-
 @if (!empty($site->ga))
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create','{{ $site->ga }}','auto'); ga('send','pageview');
-</script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{$site->ga}}"></script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','{{$site->ga}}');</script>
 @endif
