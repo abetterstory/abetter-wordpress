008d7a79377ae496899bdd07f4b66e33eb1439ef
Johan Sjöland <johansjoland@johansmacbook.local>
2018-12-22 12:50:28 +0100

    use _wp_field in Posts

--- src/Posts.php
+++ src/Posts.php
@@ -142,11 +142,11 @@ class Posts {
 		$item->datelong = (string) get_the_date('F j, Y',$post);
 		// ---
 		$item->title = (string) htmlspecialchars_decode($post->post_title);
-		$item->label = (string) ($f = get_field('teaser_label',$post)) ? $f : get_field('cover_label',$post);
-		$item->headline = (string) ($f = get_field('teaser_headline',$post)) ? $f : get_field('cover_headline',$post);
-		$item->lead = (string) ($f = get_field('teaser_excerpt',$post)) ? $f : get_field('cover_lead',$post);
+		$item->label = (string) ($f = _wp_field('teaser_label',$post)) ? $f : _wp_field('cover_label',$post);
+		$item->headline = (string) ($f = _wp_field('teaser_headline',$post)) ? $f : _wp_field('cover_headline',$post);
+		$item->lead = (string) ($f = _wp_field('teaser_excerpt',$post)) ? $f : _wp_field('cover_lead',$post);
 		$item->excerpt = (string) trim(strip_tags($item->lead));
-		$item->content = (string) $post->post_content;
+		$item->content = (string) _wp_content($post);
 		if (!$item->headline && preg_match('/<h1[^>]*>([^<]+)<\/h1>(.*)/i',$item->content,$match)) {
 			$item->headline = trim(strip_tags($match[1]));
 			if ($match[2]) $item->content = trim($match[2]);
@@ -158,12 +158,12 @@ class Posts {
 		if (!$item->headline) $item->headline = $item->title;
 		if (!$item->excerpt) $item->excerpt = _excerpt(($item->lead)?$item->lead:$item->content,400);
 		// ---
-		$item->image = (string) ($f = get_field('teaser_image',$post)) ? $f : (($f = get_field('cover_image',$post)) ? $f : '');
+		$item->image = (string) ($f = _wp_field('teaser_image',$post)) ? $f : (($f = _wp_field('cover_image',$post)) ? $f : '');
 		if (!$item->image && preg_match('/<img[^>]*src="([^"]+)"/i',$item->content,$match)) {
 			$item->image = $match[1];
 		}
 		// ---
-		$item->link = (string) ($f = get_field('teaser_link',$post)) ? $f : get_field('cover_link',$post);
+		$item->link = (string) ($f = _wp_field('teaser_link',$post)) ? $f : _wp_field('cover_link',$post);
 		if (!$item->link) $item->link = (string) ($d = _dictionary($post->post_type.'_more',NULL,'')) ? $d : _dictionary('post_more',NULL,'');
 		// ---
 		return $item;
