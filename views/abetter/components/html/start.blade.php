8c62b186433adbca21a18004d0c7ffc058424527
Johan Sjöland <johansjoland@johansmacbook.local>
2019-01-15 22:00:22 +0100

    Added global __env to start view for _render components

--- views/abetter/components/html/start.blade.php
+++ views/abetter/components/html/start.blade.php
@@ -1,4 +1,5 @@
 <!doctype html>
+@php global $___env; $___env = $__env; @endphp
 <html @include('components.html.attr')>
     <head>@include('components.html.head')</head>
     <body @include('components.html.body.attr')>
