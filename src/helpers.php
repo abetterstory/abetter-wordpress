1eeae276662854371a29d635de7539346de81102
Johan Sjöland <johansjoland@johansmacbook.local>
2019-01-22 11:08:35 +0100

    Moved wp helpers to wp

--- src/helpers.php
+++ src/helpers.php
@@ -51,3 +51,73 @@ if (! function_exists('___')) {
     }
 }
 */
+
+// ---
+
+if (!function_exists('_wp_post')) {
+
+	function _wp_page($id,$lang=NULL) {
+		return \ABetter\Wordpress\Post::getPage($id,$lang);
+	}
+
+	function _wp_post($id,$lang=NULL) {
+		return \ABetter\Wordpress\Post::getPage($id,$lang);
+	}
+
+}
+
+if (!function_exists('_wp_content')) {
+
+	function _wp_content($post,$lang=NULL,$return=NULL) {
+		$return = $post->post_content ?? ""; // Current
+		if ($lang === FALSE || empty($post->l10n->translations)) return _render($return); // No WPML
+		if ($lang && ($id = $post->l10n->translations[$lang] ?? NULL) && ($req = get_post($id))) {
+			$return = ($f = $req->post_content ?? "") ? $f : $return; // Lang
+		}
+		if (!$return && ($id = $post->l10n->translations[$post->l10n->default] ?? NULL) && ($def = get_post($id))) {
+			$return = ($f = $def->post_content ?? "") ? $f : $return; // Default
+		}
+		return _render($return); // Fallback
+	}
+
+}
+
+if (!function_exists('_wp_field')) {
+
+	function _wp_field($key,$post,$lang=NULL,$return=NULL) {
+		$return = ($f = get_field($key,$post)) ? $f : $return; // Current
+		if ($lang === FALSE || empty($post->l10n->translations)) return _render($return); // No WPML
+		if ($lang && ($id = $post->l10n->translations[$lang] ?? NULL) && ($req = get_post($id))) {
+			$return = ($f = get_field($key,$req)) ? $f : $return; // Lang
+		}
+		if (!$return && ($id = $post->l10n->translations[$post->l10n->default] ?? NULL) && ($def = get_post($id))) {
+			$return = ($f = get_field($key,$def)) ? $f : $return; // Default
+		}
+		return _render($return); // Fallback
+	}
+
+}
+
+if (!function_exists('_wp_option')) {
+
+	function _wp_option($key) {
+		return ($var = $GLOBALS['wpdb']->get_var('SELECT option_value FROM wp_options WHERE option_name = "'.$key.'"')) ? $var : NULL;
+	}
+
+}
+
+if (!function_exists('_wp_url')) {
+
+	function _wp_url($post) {
+		return _relative(get_permalink($post));
+	}
+
+}
+
+if (!function_exists('_wp_title')) {
+
+	function _wp_title($post) {
+		return $post->post_title ?? "";
+	}
+
+}
