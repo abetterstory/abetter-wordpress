70aa3f275e9daf8765f568cf7af31bc9a253aae0
Johan Sjöland <johansjoland@johansmacbook.local>
2019-02-24 15:28:28 +0100

    Removed auto render on wp_content och wp_field

--- src/helpers.php
+++ src/helpers.php
@@ -70,14 +70,14 @@ if (!function_exists('_wp_content')) {
 
 	function _wp_content($post,$lang=NULL,$return=NULL) {
 		$return = $post->post_content ?? ""; // Current
-		if ($lang === FALSE || empty($post->l10n->translations)) return _render($return); // No WPML
+		if ($lang === FALSE || empty($post->l10n->translations)) return $return; // No WPML
 		if ($lang && ($id = $post->l10n->translations[$lang] ?? NULL) && ($req = get_post($id))) {
 			$return = ($f = $req->post_content ?? "") ? $f : $return; // Lang
 		}
 		if (!$return && ($id = $post->l10n->translations[$post->l10n->default] ?? NULL) && ($def = get_post($id))) {
 			$return = ($f = $def->post_content ?? "") ? $f : $return; // Default
 		}
-		return _render($return); // Fallback
+		return $return; // Fallback
 	}
 
 }
@@ -86,14 +86,14 @@ if (!function_exists('_wp_field')) {
 
 	function _wp_field($key,$post,$lang=NULL,$return=NULL) {
 		$return = ($f = get_field($key,$post)) ? $f : $return; // Current
-		if ($lang === FALSE || empty($post->l10n->translations)) return _render($return); // No WPML
+		if ($lang === FALSE || empty($post->l10n->translations)) return $return; // No WPML
 		if ($lang && ($id = $post->l10n->translations[$lang] ?? NULL) && ($req = get_post($id))) {
 			$return = ($f = get_field($key,$req)) ? $f : $return; // Lang
 		}
 		if (!$return && ($id = $post->l10n->translations[$post->l10n->default] ?? NULL) && ($def = get_post($id))) {
 			$return = ($f = get_field($key,$def)) ? $f : $return; // Default
 		}
-		return _render($return); // Fallback
+		return $return; // Fallback
 	}
 
 }
