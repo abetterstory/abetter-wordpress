9aef8f2083112028ee953663345ea0019a242efc
Johan Sjöland <johansjoland@johansmacbook.local>
2019-04-18 21:52:35 +0200

    Added Canvas to core templates

--- resources/templates.php
+++ resources/templates.php
@@ -5,6 +5,7 @@ Wordpress theme templates
 */
 
 add_filter('theme_page_templates', function($templates){
+	$templates['canvas.php'] = 'Canvas';
 	$templates['error.php'] = 'Error';
 	$templates['front.php'] = 'Front';
 	$templates['posts.php'] = 'Posts';
