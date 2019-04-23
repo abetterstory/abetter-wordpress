a7beae18c9b5f1e5ddc85495931d4d05a699d6de
Johan Sjöland <johansjoland@johansmacbook.local>
2019-04-23 10:25:11 +0200

    Modify editor js

--- themes/abetter/editor.js
+++ themes/abetter/editor.js
@@ -31,29 +31,36 @@ function emptyNodeClass(node) {
 }
 
 function directiveNode(node) {
-	if (!node || node.nodeName.match(/body|html/i) || !node.textContent.match(/@/)) return;
-	if (!node.textContent.match(/@(end|block|classname|slot|component)/i)) return;
-	var content = node.innerHTML.trim();
-	if (content.match(/@/g).length > 1) {
-		var parent = node.parentNode;
-		var fix = content.replace(/@([^@<]+)/g,'<p>@$1</p>');
-		node.insertAdjacentHTML('beforebegin',fix);
-		parent.removeChild(node);
-		for (var i = 0; i < parent.children.length; i++) {
-			directiveNode(parent.children[i]);
+	if (!node || node.nodeName.match(/body|html/i)) return;
+	if (node.textContent.match(/@(end|block|classname|slot|component)/i)) {
+		var content = node.innerHTML.trim();
+		if (content.match(/@/g).length > 1) {
+			var parent = node.parentNode;
+			var fix = content.replace(/@([^@<]+)/g,'<p>@$1</p>');
+			try {
+				node.insertAdjacentHTML('beforebegin',fix);
+				parent.removeChild(node);
+			} catch(e) {};
+			if (parent && parent.children) {
+				for (var i = 0; i < parent.children.length; i++) {
+					directiveNode(parent.children[i]);
+				}
+			}
+		} else {
+			var klass = ''; //'--directive';
+			if (content.match(/^@(endcomponent|component)/i)) {
+				klass = '--component';
+			} else if (content.match(/^@(endslot|slot)/i)) {
+				klass = '--slot';
+			} else if (content.match(/^@(classname)/i)) {
+				klass = '--var';
+			} else if (content.match(/^@(endblock|block)/i)) {
+				klass = '--block';
+			}
+			if (!klass || node.classList.contains(klass)) return;
+			node.classList.add(klass);
 		}
 	} else {
-		var klass = ''; //'--directive';
-		if (content.match(/^@(endcomponent|component)/i)) {
-			klass = '--component';
-		} else if (content.match(/^@(endslot|slot)/i)) {
-			klass = '--slot';
-		} else if (content.match(/^@(classname)/i)) {
-			klass = '--var';
-		} else if (content.match(/^@(endblock|block)/i)) {
-			klass = '--block';
-		}
-		if (!klass || node.classList.contains(klass)) return;
-		node.classList.add(klass);
+		node.classList.remove('--component','--slot','--var','--block');
 	}
 }
